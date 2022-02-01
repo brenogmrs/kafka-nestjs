5cabb7b0d88dd0c8e0000330a4a535cf24a4a373
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-31 23:07:10 -0300

    feat: add kafka config and connection to producer

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Order } from './entities/order.entity';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Order])],
+  imports: [
+    SequelizeModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:9092'],
+          },
+          consumer: {
+            groupId: 'payments_group_producer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    OrdersService,
+    {
+      provide: 'KAFKA_PRODUCER',
+      useFactory: async (kafkaService: ClientKafka) => {
+        return kafkaService.connect();
+      },
+      inject: ['KAFKA_SERVICE'],
+    },
+  ],
 })
 export class OrdersModule {}
